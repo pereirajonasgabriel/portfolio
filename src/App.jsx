634bfbfa0ccfecd5c060828ae3fdaadb4b894fe3
pereirajonasgabriel <pereirajonasgabriel@gmail.com>
2026-01-16 11:13:09 -0300

    added some styles and create the theme toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { Home } from "lucide-react"
+
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { NotFound } from "./pages/NotFound"
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home></Home>}></Route>
-          <Route path="*" element={<NotFound></NotFound>}></Route>
+          <Route index element={<Home />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
     </>
